f9cc2870481be7602b67d9d0a9f3238e64478793
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-04-22 18:16:35 +0200

    Remove `--onefile` parameter from build (#21)
    * change working directory to script's location
    
    * Fixes

--- updater.py
+++ updater.py
@@ -8,37 +8,48 @@ import platform
 from subprocess import Popen
 
 
+# change working directory to script's location
+if getattr(sys, 'frozen', False):
+    # If the application is run as a bundle, the PyInstaller bootloader
+    # extends the sys module by a flag frozen=True and sets the app 
+    # path into variable _MEIPASS'.y
+    running_updater_path = sys._MEIPASS
+else:
+    running_updater_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(running_updater_path)
+
+
 def download_zip(url: str):
     print('Downloading zip from {0}...'.format(url))
-    urllib.request.urlretrieve(url, "Updater/archive.zip")
+    urllib.request.urlretrieve(url, "archive.zip")
 
 
 def backup_config(backend_folder: str):
     print('Backing up configuration...')
-    config_path = backend_folder + "/configuration.txt"
-    config_backup_path = 'Updater/' + config_path
+    config_backup_path = os.path.join(backend_folder, "configuration.txt")
+    config_path = os.path.join('./../', config_backup_path)
     if os.path.exists(config_path):
-        if not os.path.exists('Updater/'+backend_folder):
-            os.makedirs('Updater/'+backend_folder)
+        if not os.path.exists(backend_folder):
+            os.makedirs(backend_folder)
         shutil.copyfile(config_path, config_backup_path)
 
 
 def restore_config(backend_folder: str):
     print('Restoring configuration...')
-    config_file_path = backend_folder + "/configuration.txt"
-    config_backup_path = 'Updater/' + config_file_path
+    config_backup_path = os.path.join(backend_folder, "configuration.txt")
+    config_path = os.path.join('./../', config_backup_path)
     if os.path.exists(config_backup_path):
-        shutil.copyfile(config_backup_path, config_file_path)
+        shutil.copyfile(config_backup_path, config_path)
 
 
 def clear_converter_folder():
     print('Clearing converter folder...')
 
-    paths = glob.glob('./*', recursive=False)
+    paths = glob.glob('../*', recursive=False)
     path: str
     for path in paths:
         if os.path.isdir(path):
-            if path == '.\\Updater':
+            if os.path.samefile(path, running_updater_path):
                 continue
             shutil.rmtree(path, ignore_errors=True)
         else:
@@ -47,11 +58,13 @@ def clear_converter_folder():
 
 def extract_zip():
     print('Extracting zip...')
-    with zipfile.ZipFile('Updater/archive.zip', 'r') as zip_ref:
-        zip_ref.extractall('.')
+    with zipfile.ZipFile("archive.zip", 'r') as zip_ref:
+        zip_ref.extractall('..')
 
 
 def open_frontend():
+    os.chdir('../')
+
     # https://stackoverflow.com/a/13256908/10249243
     # set system/version dependent "start_new_session" analogs
     kwargs = {}
@@ -74,7 +87,7 @@ if len(sys.argv) != 3:
 
 converterZipURL = sys.argv[1]
 converterBackendFolder = sys.argv[2]
-if not os.path.isdir(converterBackendFolder):
+if not os.path.isdir(os.path.join('..', converterBackendFolder)):
     print('Converter backend folder {0} does not exist!'.format(converterBackendFolder))
     sys.exit(2)
 
